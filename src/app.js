3e23631246b93aec1ebcb9235c1ee2660012d4d8
Akira <54892411+docxed@users.noreply.github.com>
2024-05-10 00:08:20 +0700

    add cors config

--- src/app.js
+++ src/app.js
@@ -13,7 +13,12 @@ connectDB()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  }),
+)
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'))
 
 const errorException = require('./middlewares/errorException')
